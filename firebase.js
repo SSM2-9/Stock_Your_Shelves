0d0f172ff29eabb7bf138e1d4bde8a7b4d020310
Sakina <140181710+SSM2-9@users.noreply.github.com>
2024-09-28 07:42:53 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import Firebase SDK and Firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDaAuum7Imm26BFr2F1-t98CyiC8wnFGiQ",
   authDomain: "inventory-management-app-1ea8a.firebaseapp.com",
@@ -10,7 +12,10 @@ const firebaseConfig = {
   appId: "1:776870885738:web:861a6ef10c17ae657dcd13"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore (add this)
 const firestore = getFirestore(app);
 
 export { firestore };
