5f8aaf64a0475d0bbd938223c206e5cd9aa1107a
Sakina <140181710+SSM2-9@users.noreply.github.com>
2024-09-28 07:30:03 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -8,14 +10,8 @@ const firebaseConfig = {
   projectId: "inventory-management-app-1ea8a",
   storageBucket: "inventory-management-app-1ea8a.appspot.com",
   messagingSenderId: "776870885738",
-  appId: "1:776870885738:web:861a6ef10c17ae657dcd13",
-  measurementId: "G-7ZTZVNF3W4"
+  appId: "1:776870885738:web:861a6ef10c17ae657dcd13"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
-const firestore = getFirestore(app);
-
-export { app, firestore };
